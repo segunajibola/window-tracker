edf9530756243b9331afd948c98b00ebee3eda31
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-31 23:42:57 +0100

    firstCommit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <div className='p-10'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className='text-2xl'>Window Tracker</h1>
+        <p className='text-xl text-center'>Check width of any device</p>
+
+        <button className='rounded-lg p-5'>Check width of this device</button>
+    </div>
+  )
+}
+
+export default Home
