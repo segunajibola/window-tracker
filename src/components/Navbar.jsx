475dc348ac5ca0958819a79281218577b8cc23fc
Segun Ajibola <ajbl.segun@gmail.com>
2023-01-01 01:08:32 +0100

    firstCommit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa"
 import "../navbar.css"
 
 const Navbar = (props) => {
   return (
     <nav className={`flex justify-between p-5 shadow-md ${props.darkMode ? "dark" : ""} dark:bg-gray-900`}>
       <h1 className="text-lg font-semibold dark:text-gray-100">Window Width</h1>
-      <span className="text-md dark:text-gray-100">Dark Mode</span>
+      <FaGithub />
       <div className="toggler">
         <p className="toggler--light">Light</p>
         <div className="toggler--slider" onClick={props.toggleDarkMode}>
