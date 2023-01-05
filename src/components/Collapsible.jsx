b4cbeb6c39010848d796a78133ec70342aae374a
Segun Ajibola <ajbl.segun@gmail.com>
2023-01-05 23:59:22 +0100

    editCollapsibleData

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -6,9 +6,7 @@ const Collapsible = () => {
     <div className="border-2 text-gray-500 w-[80%] mx-auto">
       <div className="container mx-auto py-2 px-2">
         <details className="bg-gray-200 shadow rounded group mb-2">
-          <summary
-            className="list-none flex flex-wrap items-center cursor-pointer focus-visible:outline-none focus-visible:ring focus-visible:ring-pink-500 rounded group-open:rounded-b-none group-open:z-[1] relative"
-          >
+          <summary className="list-none flex flex-wrap items-center cursor-pointer focus-visible:outline-none focus-visible:ring focus-visible:ring-pink-500 rounded group-open:rounded-b-none group-open:z-[1] relative">
             <h3 className="flex flex-1 p-3 font-semibold">
               {collapsible.name}
             </h3>
